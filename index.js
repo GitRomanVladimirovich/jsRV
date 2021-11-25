5ba99555391010d47b49fdd5be21c8ae64da0941
GitRomanVladimirovich <GitRomanVladimirovich@users.noreply.github.com>
2021-11-25 20:20:39 +0000

    Deploying to gh-pages from @ GitRomanVladimirovich/jsRV@f511c30d12bf26046007beb66e74ee7e363b5557 🚀

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-"use strict"
-let product = "Кресло";
-let price = 12000;
-let result = product+", цена "+ price+" рублей";
-alert(result);
+let min = Number(prompt('Введите min число:'));
+let max = Number(prompt('Введите max число:'));
+
+function randomNumber(min, max) {
+  const r = Math.random()*(max-min) + min
+  return Math.floor(r)
+}
+
+// eslint-disable-next-line
+console.log(randomNumber(min, max));
